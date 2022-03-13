ffffb603b7c1593540570d8ad7da75ddecc9ded0
MdbabulHossen <99380002+MdbabulHossen@users.noreply.github.com>
2022-03-13 16:45:24 +0600

    add

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middleware=jsonServer.defaults({
     static:"./build",
 });
 const cors=require("cors")
-const port=5000;
+const port=process.env.PORT || 5000;
 server.use(middleware);
 server.use(jsonServer.rewriter({
     "/api/*":"/$1",
