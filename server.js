e955d35582fef1979d1b03ef6519de12abd05584
MdbabulHossen <99380002+MdbabulHossen@users.noreply.github.com>
2022-03-13 16:39:51 +0600

    add

---
+++ server.js
@@ -0,0 +1,18 @@
+const jsonServer=require("json-server");
+const server=jsonServer.create();
+const router=jsonServer.router("./db.json");
+const middleware=jsonServer.defaults({
+    static:"./build",
+});
+const cors=require("cors")
+const port=5000;
+server.use(middleware);
+server.use(jsonServer.rewriter({
+    "/api/*":"/$1",
+}));
+
+server.use(router);
+server.use(cors())
+server.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+})
